cd31eeb32c9a94688d11145ed3eceac90d43a6b3
Mehdi Azadi <azadi.mehdi@gmail.com>
2018-12-11 10:32:09 +0330

    'commited-from-olive-microservice-explorer.'

--- Website/Controllers/SharedActionsController.cs
+++ Website/Controllers/SharedActionsController.cs
@@ -11,7 +11,7 @@
 
     public class SharedActionsController : BaseController
     {
-        [Route("error1 131111")]
+        [Route("error1 1311111")]
         public ActionResult Error() => View("error");
 
         [Route("error/404")]
